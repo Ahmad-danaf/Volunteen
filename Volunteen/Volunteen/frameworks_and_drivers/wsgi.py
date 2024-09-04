ea5e51cfe4ff080b1681c21e11bcc35c6afde839
adamTakrury <119703433+adamTakrury@users.noreply.github.com>
2024-09-04 15:19:07 +0300

    Update wsgi.py

--- Volunteen/Volunteen/frameworks_and_drivers/wsgi.py
+++ Volunteen/Volunteen/frameworks_and_drivers/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Volunteen.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Volunteen.frameworks_and_drivers.settings')
 
 application = get_wsgi_application()
