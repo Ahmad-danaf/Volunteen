021f0338902c42f50d69b51a6c107b4d54e17bc5
adamTakrury <119703433+adamTakrury@users.noreply.github.com>
2024-09-05 15:52:26 +0300

    Update settings.py

--- Volunteen/Volunteen/frameworks_and_drivers/settings.py
+++ Volunteen/Volunteen/frameworks_and_drivers/settings.py
@@ -145,7 +145,7 @@ import os
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # מיקום לשמירת הקבצים שנאספים עם collectstatic
 
-STATIC_URL = '/static/'
+STATIC_URL = '../../static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
