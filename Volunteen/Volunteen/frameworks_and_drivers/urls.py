60288ca4904112c01447cfe4e8ed6fdc58e11eca
Ahmad Danaf <122477841+Ahmad-danaf@users.noreply.github.com>
2024-09-05 17:01:25 +0300

    Update urls.py

--- Volunteen/Volunteen/frameworks_and_drivers/urls.py
+++ Volunteen/Volunteen/frameworks_and_drivers/urls.py
@@ -33,5 +33,5 @@ urlpatterns = [
     # 2FA
     path('', include(tf_urls)),
 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
