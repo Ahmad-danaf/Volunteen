9b1fe3d5b7a7ef4fc674464fe5e4c0675785d3ea
adamTakrury <119703433+adamTakrury@users.noreply.github.com>
2024-09-05 16:31:29 +0300

    Update settings.py

--- Volunteen/Volunteen/frameworks_and_drivers/settings.py
+++ Volunteen/Volunteen/frameworks_and_drivers/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
