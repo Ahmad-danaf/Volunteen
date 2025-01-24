69374c194e37079bdb914b1f3207181820d7d4c8
Ahmad Danaf <ahmad.danf@gmail.com>
2025-01-24 13:16:23 +0200

    Add parent home, child detail and task dashboard views for parantApp, update URLs, and enhance parent admin

--- Volunteen/parentApp/admin.py
+++ Volunteen/parentApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Parent
 # Register your models here.
+
+
+class ParentAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+    search_fields = ('user__username', 'user__email')
+
+admin.site.register(Parent, ParentAdmin)
