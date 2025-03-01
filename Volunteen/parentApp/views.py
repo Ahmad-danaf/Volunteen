7cca4c1f2b7c99a6ab79ce3278cba667b8bba736
Ahmad Danaf <ahmad.danf@gmail.com>
2025-03-01 09:54:43 +0200

    Refactor import paths for LeaderboardUtils in child and parent views

--- Volunteen/parentApp/views.py
+++ Volunteen/parentApp/views.py
@@ -13,7 +13,7 @@ from django.utils.timezone import now
 from django.templatetags.static import static
 from shopApp.models import Shop, Reward, Redemption, Category
 from Volunteen.constants import AVAILABLE_CITIES
-from Volunteen.childApp.utils.LeaderboardUtils import LeaderboardUtils
+from childApp.utils.LeaderboardUtils import LeaderboardUtils
 from teenApp.interface_adapters.forms import DateRangeCityForm
 
 def parent_landing(request):
