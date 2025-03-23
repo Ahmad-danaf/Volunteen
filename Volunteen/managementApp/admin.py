62817124f98870fd8ca9c20dfd14e5fc6352bf2e
Ahmad Danaf <ahmad.danf@gmail.com>
2025-03-23 05:53:15 +0200

    Enhance managementApp admin and templates
    - Added DonationSpending and SpendingAllocation admin classes for better management of expenditures.
    - Updated donation_base.html and donation_dashboard.html to include Font Awesome icons for improved UI and added logout functionality in the navigation.

--- Volunteen/managementApp/admin.py
+++ Volunteen/managementApp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import DonationCategory, DonationTransaction
+from .models import DonationCategory, DonationTransaction, DonationSpending, SpendingAllocation
 
 class DonationCategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'is_active')
@@ -9,7 +9,20 @@ class DonationTransactionAdmin(admin.ModelAdmin):
     list_display = ('child', 'category', 'amount', 'date_donated')
     list_filter = ('category', 'date_donated')
     search_fields = ('child__name', 'category__name')
-
+    
+    
+class DonationSpendingAdmin(admin.ModelAdmin):
+    list_display = ('category', 'amount_spent', 'date_spent')
+    list_filter = ('category', 'date_spent')
+    search_fields = ('category__name',)
+    
+class SpendingAllocationAdmin(admin.ModelAdmin):
+    list_display = ('spending', 'transaction', 'amount_used')
+    list_filter = ('spending__category', 'transaction__child')
+    search_fields = ('spending__category__name', 'transaction__child__name')
+    
 admin.site.register(DonationCategory, DonationCategoryAdmin)
 admin.site.register(DonationTransaction, DonationTransactionAdmin)
+admin.site.register(DonationSpending, DonationSpendingAdmin)
+admin.site.register(SpendingAllocation, SpendingAllocationAdmin)
 
